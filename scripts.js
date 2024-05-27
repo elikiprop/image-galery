048b3b10d6d0e31f7b42028b5f283ce708c27d3c
Eli Kiprop <elikiprop42@gmail.com>
2024-05-27 13:28:55 +0300

    update the changes

--- scripts.js
+++ scripts.js
@@ -1,29 +1,55 @@
 document.addEventListener('DOMContentLoaded', function() {
     const gallery = document.querySelector('.gallery');
-    const images = [
-        'images/1.jpeg',
-        'images/2.jpeg',
-        'images/3.jpeg',
-        'images/4.jpeg',
-        'images/5.jpeg',
-        'images/6.jpeg',
-        'images/7.jpeg',
-        'images/Mekatilili.jpg',
-        'images/8.jpeg',
-        'images/9.jpeg',
-        'images/10.jpeg',
-        'images/11.jpeg',
-        'images/12.jpeg',
-        'images/13.jpeg',
-        'images/14.jpeg'
+    const yearSelect = document.getElementById('yearSelect');
+
+    const imagesByYear = {
+        2023: [
+            'images/2023/1.jpeg',
+            'images/2023/2.jpeg',
+            'images/2023/3.jpeg',
+            'images/2023/4.jpeg',
+            'images/2023/5.jpeg',
+            'images/2023/6.jpeg',
+            'images/2023/7.jpeg',
+        ],
+        2024: [
+            'images/2024/8.jpeg',
+            'images/2024/9.jpeg',
+            'images/2024/10.jpeg',
+            'images/2024/11.jpeg',
+            'images/2024/12.jpeg',
+            'images/2024/13.jpeg',
+            'images/2024/14.jpeg',
+            'images/2024/Mekatilili.jpg',
+            'images/2024/15.jpeg',
+            'images/2024/16.jpeg',
+            'images/2024/17.jpeg',
+            'images/2024/18.jpeg',
 
 
-        // Add more image paths here
-    ];
 
-    images.forEach(src => {
-        const img = document.createElement('img');
-        img.src = src;
-        gallery.appendChild(img);
+        ],
+        2025: []
+    
+    };
+
+    function loadImages(year) {
+        gallery.innerHTML = '';
+        const images = imagesByYear[year];
+        if (images && images.length > 0) {
+            images.forEach(src => {
+                const img = document.createElement('img');
+                img.src = src;
+                gallery.appendChild(img);
+            });
+        } else {
+            gallery.innerHTML = '<p><h2>Coming soon</h2>.</p>';
+        }
+    }
+
+    loadImages(yearSelect.value);
+
+    yearSelect.addEventListener('change', function() {
+        loadImages(yearSelect.value);
     });
 });
